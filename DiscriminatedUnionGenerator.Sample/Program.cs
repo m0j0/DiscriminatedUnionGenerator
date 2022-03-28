ba44d160cedc07c2f88a6607957689e7236e8010
m0j0 <maslovn@gmail.com>
2022-03-28 16:07:04 +0300

    проект с примером

---
+++ DiscriminatedUnionGenerator.Sample/Program.cs
@@ -0,0 +1,10 @@
+namespace DiscriminatedUnionGenerator.Sample
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
